019be2b2b927778204b215bca7ec46d02418562c
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-05-28 12:23:29 +0200

    extract error if exists test

---
+++ abstract/error-if-exists-test.js
@@ -0,0 +1,9 @@
+module.exports = function (test, level, location) {
+  test('test db open and use, level(location, options, cb) force error', function (t) {
+    level(location, { errorIfExists: true }, function (err, db) {
+      t.ok(err, 'got error opening existing db')
+      t.notOk(db, 'no db')
+      t.end()
+    })
+  })
+}
--- abstract/test.js
+++ abstract/test.js
@@ -85,14 +85,7 @@ module.exports = function (test, level, options) {
   })
 
   if (!options.skipErrorIfExistsTest) {
-    // should use existing options object
-    test('test db open and use, level(location, options, cb) force error', function (t) {
-      level(location, { errorIfExists: true }, function (err, db) {
-        t.ok(err, 'got error opening existing db')
-        t.notOk(db, 'no db')
-        t.end()
-      })
-    })
+    require('./error-if-exists-test')(test, level, location)
   }
 
   if (!options.skipRepairTest) {
