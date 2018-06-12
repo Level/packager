df3de17e8bcca1250493822413638ad1de9dcc2d
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-06-12 12:30:29 +0200

    Test packager functionality using generic Down

--- test.js
+++ test.js
@@ -1,10 +1,53 @@
 'use strict'
 
-module.exports = require('./abstract/test')
+var test = require('tape')
+var packager = require('.')
 
-if (!module.parent) {
-  var test = require('tape')
-  var packager = require('./')
-  var leveldown = require('leveldown')
-  module.exports(test, packager(leveldown))
-}
+test('Level constructor relays .destroy if it exists', function (t) {
+  t.plan(2)
+  function Down () {}
+  Down.destroy = function (location, cb) {
+    t.is(location, 'location', 'location is correct')
+    t.is(typeof cb, 'function', 'cb is set')
+  }
+  packager(Down).destroy('location')
+})
+
+test('Level constructor relays .repair if it exists', function (t) {
+  t.plan(2)
+  function Down () {}
+  Down.repair = function (location, cb) {
+    t.is(location, 'location', 'location is correct')
+    t.is(typeof cb, 'function', 'cb is set')
+  }
+  packager(Down).repair('location')
+})
+
+test('Level constructor, default options', function (t) {
+  t.plan(3)
+  function Down (location) {
+    t.is(location, 'location', 'location is correct')
+    return {
+      open: function (opts, cb) {}
+    }
+  }
+  var levelup = packager(Down)('location')
+  t.is(levelup.options.keyEncoding, 'utf8')
+  t.is(levelup.options.valueEncoding, 'utf8')
+})
+
+test('Level constructor, custom options', function (t) {
+  t.plan(3)
+  var Down = function (location) {
+    t.is(location, 'location', 'location is correct')
+    return {
+      open: function (opts, cb) {}
+    }
+  }
+  var levelup = packager(Down)('location', {
+    keyEncoding: 'binary',
+    valueEncoding: 'binary'
+  })
+  t.is(levelup.options.keyEncoding, 'binary')
+  t.is(levelup.options.valueEncoding, 'binary')
+})
